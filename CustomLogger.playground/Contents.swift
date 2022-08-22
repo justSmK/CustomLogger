94b5129a9186a21df8e71d72d3a2fc80011b38e2
justSmK <semko431@gmail.com>
2022-08-22 13:57:58 +0300

    Init enum log and functions debug and error

--- CustomLogger.playground/Contents.swift
+++ CustomLogger.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+enum Log {
+    static func debug(
+        _ data: @autoclosure () -> Any?,
+        file: String = #file,
+        line: UInt = #line
+    ) {
+        
+    }
+    
+    static func error(
+        _ data: @autoclosure () -> Any?,
+        file: String = #file,
+        line: UInt = #line
+    ) {
+        
+    }
+}
+
+Log.debug(Data())
+Log.error("Error")
